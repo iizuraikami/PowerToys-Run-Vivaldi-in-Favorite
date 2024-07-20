6d7237b15811ecf1b66b44399ffb373633d0d97a
iizuraikami <168643078+iizuraikami@users.noreply.github.com>
2024-07-20 09:09:40 +0900

    Replace Vivaldi

---
+++ Community.PowerToys.Run.Plugin.VivaldiFavorite/Helpers/IFavoriteProvider.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Davide Giacometti. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Community.PowerToys.Run.Plugin.VivaldiFavorite.Models;
+
+namespace Community.PowerToys.Run.Plugin.VivaldiFavorite.Helpers
+{
+    public interface IFavoriteProvider
+    {
+        FavoriteItem Root { get; }
+    }
+}
---
+++ Community.PowerToys.Run.Plugin.VivaldiFavorite/Main.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Davide Giacometti. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using Community.PowerToys.Run.Plugin.VivaldiFavorite.Helpers;
+using Community.PowerToys.Run.Plugin.VivaldiFavorite.Models;
+using ManagedCommon;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Wox.Infrastructure;
+using Wox.Plugin;
+
+namespace Community.PowerToys.Run.Plugin.VivaldiFavorite
+{
+    public class Main : IPlugin, ISettingProvider, IContextMenu
+    {
+        public static string PluginID => "D73A7EF0633F4C82A14454FFD848F447";
+
+        private const string SearchTree = nameof(SearchTree);
+        private const bool SearchTreeDefault = false;
+        private readonly IFavoriteProvider _favoriteProvider;
+        private readonly IFavoriteQuery _favoriteQuery;
+        private PluginInitContext? _context;
+        private bool _searchTree;
+
+        public string Name => "Vivaldi Favorite";
+
+        public string Description => "Vivaldiブラウザのブックマークを検索します";
+
+        public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>
+        {
+            new PluginAdditionalOption
+            {
+                Key = SearchTree,
+                Value = SearchTreeDefault,
+                DisplayLabel = "Search as tree",
+                DisplayDescription = "Navigate the original directory tree when searching.",
+            },
+        };
+
+        public Main()
+        {
+            _favoriteProvider = new FavoriteProvider();
+            _favoriteQuery = new FavoriteQuery();
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconsPath(_context.API.GetCurrentTheme());
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var search = query.Search.Replace('\\', '/').Split('/');
+
+            if (_searchTree)
+            {
+                return _favoriteQuery
+                    .Search(_favoriteProvider.Root, search, 0)
+                    .OrderBy(f => f.Type)
+                    .ThenBy(f => f.Name)
+                    .Select(f => f.CreateResult())
+                    .ToList();
+            }
+            else
+            {
+                var results = new List<Result>();
+
+                foreach (var favorite in _favoriteQuery.GetAll(_favoriteProvider.Root))
+                {
+                    var score = StringMatcher.FuzzySearch(query.Search, favorite.Name);
+                    if (string.IsNullOrWhiteSpace(query.Search) || score.Score > 0)
+                    {
+                        var result = favorite.CreateResult();
+                        result.Score = score.Score;
+                        result.TitleHighlightData = score.MatchData;
+                        results.Add(result);
+                    }
+                }
+
+                return results.OrderBy(r => r.Title).ToList();
+            }
+        }
+
+        public Control CreateSettingPanel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSettings(PowerLauncherPluginSettings settings)
+        {
+            if (settings != null && settings.AdditionalOptions != null)
+            {
+                _searchTree = settings.AdditionalOptions.FirstOrDefault(x => x.Key == SearchTree)?.Value ?? SearchTreeDefault;
+            }
+            else
+            {
+                _searchTree = SearchTreeDefault;
+            }
+        }
+
+        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+        {
+            if (selectedResult.ContextData is not FavoriteItem favorite)
+            {
+                return new List<ContextMenuResult>();
+            }
+
+            return favorite.CreateContextMenuResult();
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconsPath(newTheme);
+        }
+
+        private static void UpdateIconsPath(Theme theme)
+        {
+            FavoriteItem.SetIcons(theme);
+        }
+    }
+}
